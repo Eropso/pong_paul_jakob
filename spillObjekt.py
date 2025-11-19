93b8a905b3877021da2c379aa6e374c043f4530c
JakobGit <jakob.seljelid@gmail.com>
2025-11-19 09:11:16 +0100

    noe

--- spillObjekt.py
+++ spillObjekt.py
@@ -1,4 +1,4 @@
-class spillObjekt():
+class spillObjekt:
     def __init__(self, posisjon_x: int, posisjon_y: int, storrelse_x: int, storrelse_y: int):
         self._posisjon_x = posisjon_x
         self._posisjon_y = posisjon_y

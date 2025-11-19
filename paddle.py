c01ae66f8461483217d59076713be5328ef38d4a
Eropso <156892352+Eropso@users.noreply.github.com>
2025-11-19 09:44:10 +0100

    arve storrelse_x og storrelse_y fra spillobjekt

--- paddle.py
+++ paddle.py
@@ -1,8 +1,8 @@
 from spillObjekt import SpillObjekt
 
 class Paddle(SpillObjekt):
-    def __init__(self, posisjon_x:int, posisjon_y:int, hastighet:int):
-        super().__init__(posisjon_x, posisjon_y)
+    def __init__(self, posisjon_x:int, posisjon_y:int, storrelse_x:int, storrelse_y:int, hastighet:int):
+        super().__init__(posisjon_x, posisjon_y, storrelse_x, storrelse_y)
         self.hastighet = hastighet
 
         

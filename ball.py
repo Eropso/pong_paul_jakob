60f162a789ed1a4c7023870611008a074b38664f
JakobGit <jakob.seljelid@gmail.com>
2025-11-19 09:17:36 +0100

    lage ball klasse

---
+++ ball.py
@@ -0,0 +1,7 @@
+from spillObjekt import SpillObjekt
+
+class Ball(SpillObjekt):
+    def __init__(self, posisjon_x, posisjon_y, storrelse_x, storrelse_y, hastighet_x, hastighet_y):
+        super().__init__(posisjon_x, posisjon_y, storrelse_x, storrelse_y)
+        self._hastigeht_x = hastighet_x
+        self._hastighet_y = hastighet_y

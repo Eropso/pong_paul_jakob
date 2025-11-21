03c1cd102776c3b904e76a17ff2759dbc135f17b
JakobGit <jakob.seljelid@gmail.com>
2025-11-21 10:11:08 +0100

    legget til ball bevegelse metode

--- ball.py
+++ ball.py
@@ -1,9 +1,11 @@
 from spillObjekt import SpillObjekt
 
-
-
 class Ball(SpillObjekt):
     def __init__(self, posisjon_x, posisjon_y, storrelse_x, storrelse_y, hastighet_x, hastighet_y):
         super().__init__(posisjon_x, posisjon_y, storrelse_x, storrelse_y)
-        self._hastigeht_x = hastighet_x
+        self._hastighet_x = hastighet_x
         self._hastighet_y = hastighet_y
+
+    def oppdater(self):
+        self.pos_x += self._hastighet_x
+        self.pos_y += self._hastighet_y
